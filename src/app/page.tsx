e8af83e27781dc224e6ded5f85097fa5570e664a
adrian.czesnowski <adrian.czesnowski22@gmail.com>
2024-08-06 08:21:27 +0200

    working on a new website design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,126 @@
-import Image from "next/image";
+import {WorkCard} from "@/components/work-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import {Badge} from "@/components/ui/badge";
+import {DATA} from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
 
-export default function Home() {
-  return (
-      <section className="relative flex flex-col gap-4">
-          <h1 className="font-medium text-3xl mb-8 tracking-tighter">
-              hi, I&apos;m Adrian 👋
-          </h1>
-            <div>
-              <Image
-                  src={'/adrian-czesnowski2.jpg'}
-                  alt='Adrian Czesnowski - my photo'
-                  width={400}
-                  height={1000}
-                  quality={100}
-                  style={{
-                      width: '100%',
-                      height: 'auto',
-                      maxWidth: '400px',
-                  }}
-              />
-            </div>
-          <p className="prose prose-neutral dark:prose-invert">
-              I&apos;m a developer specializing in <strong>Flutter</strong> & <strong>React</strong>.
-              I also work with <strong>React Native</strong> and <strong>Vue</strong>.
-          </p>
-          <p>In my free time, I learn Kotlin and spend time at the <strong>gym</strong> 🏋️‍♂️</p>
-          <p>
-              This is my temporary website, in my spare time I will take care of it to make it nicer here 😅
-          </p>
-          <p>Feel free to contact me:</p>
-          <ul className='flex flex-row gap-4 underline'>
-              <li><a href="mailto:adrian.czesnowski22@gmail.com" className='py-4' rel="noopener noreferrer">E-mail</a></li>
-              <li><a href="https://www.linkedin.com/in/adrian-czesnowski/" target="_blank" className='py-4' rel="noopener noreferrer">LinkedIn</a></li>
-              <li><a href="https://github.com/adrianczesnowski" target="_blank" className='py-4' rel="noopener noreferrer">GitHub</a></li>
-          </ul>
-      </section>
-);
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+    return (
+        <main className="flex flex-col min-h-[100dvh] space-y-10">
+            <section id="hero">
+                <div className="mx-auto w-full max-w-2xl space-y-8">
+                    <div className="gap-2 flex justify-between">
+                        <div className="flex-col flex flex-1 space-y-1.5">
+                            <BlurFadeText
+                                delay={BLUR_FADE_DELAY}
+                                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                                yOffset={8}
+                                text={`Hi, I'm ${DATA.name} 👋`}
+                            />
+                            <BlurFadeText
+                                className="max-w-[600px] md:text-xl"
+                                delay={BLUR_FADE_DELAY}
+                                text={DATA.description}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="about">
+                <BlurFade delay={BLUR_FADE_DELAY * 3}>
+                    <h2 className="text-xl font-bold">About</h2>
+                </BlurFade>
+                <BlurFade delay={BLUR_FADE_DELAY * 4}>
+                    <Markdown
+                        className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+                        {DATA.summary}
+                    </Markdown>
+                </BlurFade>
+            </section>
+            <section id="hackathons">
+                <div className="space-y-12 w-full">
+                    <BlurFade delay={BLUR_FADE_DELAY * 13}>
+                        <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                            <div className="space-y-2">
+                                <div
+                                    className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                                    Work
+                                </div>
+                                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                                    Work Experience
+                                </h2>
+                                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                    During my several years of work as a programmer, I have worked on a wide
+                                    variety of projects, from different industries. I was always surrounded by people
+                                    from whom I could learn a lot.
+                                </p>
+                            </div>
+                        </div>
+                    </BlurFade>
+                    <BlurFade delay={BLUR_FADE_DELAY * 14}>
+                        <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+                            {DATA.work.map((project, id) => (
+                                <BlurFade
+                                    key={project.title + project.dates}
+                                    delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+                                >
+                                    <WorkCard
+                                        title={project.title}
+                                        description={project.description}
+                                        location={project.location}
+                                        dates={project.dates}
+                                        image={project?.image}
+                                        links={project.links}
+                                    />
+                                </BlurFade>
+                            ))}
+                        </ul>
+                    </BlurFade>
+                </div>
+            </section>
+            <section id="skills">
+                <div className="flex min-h-0 flex-col gap-y-3 pb-12">
+                    <BlurFade delay={BLUR_FADE_DELAY * 9}>
+                        <h2 className="text-xl font-bold">Skills</h2>
+                    </BlurFade>
+                    <div className="flex flex-wrap gap-1">
+                        {DATA.skills.map((skill, id) => (
+                            <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                                <Badge key={skill}>{skill}</Badge>
+                            </BlurFade>
+                        ))}
+                    </div>
+                </div>
+            </section>
+            <section id="contact">
+                <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+                    <BlurFade delay={BLUR_FADE_DELAY * 16}>
+                        <div className="space-y-3">
+                            <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                                Contact
+                            </div>
+                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                                Get in Touch
+                            </h2>
+                            <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                Want to chat? Just shoot me a dm{" "}
+                                <Link
+                                    href={DATA.contact.social.LinkedIn.url}
+                                    className="text-blue-500 hover:underline"
+                                >
+                                    with a direct question on twitter
+                                </Link>{" "}
+                                and I&apos;ll respond whenever I can. I will ignore all
+                                soliciting.
+                            </p>
+                        </div>
+                    </BlurFade>
+                </div>
+            </section>
+        </main>
+    );
 }
---
+++ src/data/resume.tsx
@@ -0,0 +1,85 @@
+import {Icons} from "@/components/icons";
+import {HomeIcon, NotebookIcon} from "lucide-react";
+
+export const DATA = {
+    name: "Adrian",
+    initials: "AC",
+    url: "https://imadrian.dev",
+    location: "Ostrow Wlkp.",
+    locationLink: "https://www.google.com/maps/place/sanfrancisco",
+    description:
+        "Currently Flutter developer. I love building things and helping people. Sometimes I go to the gym.",
+    summary:
+        "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    avatarUrl: "/me.jpg",
+    skills: [
+        "Flutter",
+        "Dart",
+        "Riverpod",
+        "BLoC",
+        "React",
+        "Next.js",
+        "Vue.js",
+        "Typescript",
+        "Node.js",
+    ],
+    navbar: [
+        {href: "/", icon: HomeIcon, label: "Home"},
+        // {href: "/blog", icon: NotebookIcon, label: "Blog"},
+    ],
+    contact: {
+        email: "hello@example.com",
+        tel: "+123456789",
+        social: {
+            GitHub: {
+                name: "GitHub",
+                url: "https://dub.sh/dillion-github",
+                icon: Icons.github,
+                navbar: true,
+            },
+            LinkedIn: {
+                name: "LinkedIn",
+                url: "https://dub.sh/dillion-linkedin",
+                icon: Icons.linkedin,
+                navbar: true,
+            },
+            email: {
+                name: "Send Email",
+                url: "#",
+                icon: Icons.email,
+                navbar: false,
+            },
+        },
+    },
+    work: [
+        {
+            title: "THOC Ltd.",
+            dates: "Dec 2023 - Now",
+            location: "Flutter Developer",
+            description:
+                "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
+            image:
+                "/thoc.jpeg",
+            links: [],
+        },
+        {
+            title: "Freelancer",
+            dates: "May 2023 - Dec 2023",
+            location: "React / React Native Developer",
+            description:
+                "Developed a mobile application which delivers university campus wide events in real time to all students.",
+            links: [],
+        },
+        {
+            title: "Qodeca Sp. z o.o.",
+            dates: "June 2021 - Apr 2023",
+            location: "Junior React / React Native Developer",
+            description:
+                "Developed a mobile application which communcicates a victims medical data from inside an ambulance to doctors at hospital.",
+            icon: "public",
+            image:
+                "qodeca.jpeg",
+            links: [],
+        },
+    ],
+} as const;
