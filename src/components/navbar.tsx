859f14efaa02f537c68437332ae4145526992066
adrian.czesnowski <adrian.czesnowski22@gmail.com>
2025-03-05 08:18:20 +0100

    updated bio

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,37 @@
-'use client'
+"use client";
 
-import {ConfettiButton} from "@/components/magicui/confetti";
-import {CopyIcon, Check} from "lucide-react";
-import React, {useState} from "react";
+import { ConfettiButton } from "@/components/magicui/confetti";
+import { CopyIcon, Check } from "lucide-react";
+import React, { useState } from "react";
+import { DATA } from "@/data/resume";
 
 export default function Navbar() {
-    const [isEmailCopied, setIsEmailCopied] = useState(false);
+  const [isEmailCopied, setIsEmailCopied] = useState(false);
 
-    function copyEmail() {
-        setIsEmailCopied(true);
-        navigator.clipboard.writeText("adrian.czesnowski22@gmail.com")
-            .then(() => {
-            })
-            .catch(err => console.error(err));
-        setTimeout(() => setIsEmailCopied(false), 3000);
-    }
+  function copyEmail() {
+    setIsEmailCopied(true);
+    navigator.clipboard
+      .writeText(DATA.contact.email)
+      .then(() => {})
+      .catch((err) => console.error(err));
+    setTimeout(() => setIsEmailCopied(false), 3000);
+  }
 
-    return (
-        <aside className="-ml-[8px] mb-16 tracking-tight">
-            <div className="lg:sticky lg:top-20">
-                <nav
-                    className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-                    id="nav"
-                >
-                    <div className="flex flex-row gap-2 justify-end space-x-0 w-full">
-                        <ConfettiButton onButtonClick={copyEmail}>
-                            <span>Get my email</span>
-                            {isEmailCopied ? <Check size={16}/> : <CopyIcon size={16}/>}
-                        </ConfettiButton>
-                    </div>
-                </nav>
-            </div>
-        </aside>
-    );
+  return (
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <div className="lg:sticky lg:top-20">
+        <nav
+          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          id="nav"
+        >
+          <div className="flex flex-row gap-2 justify-end space-x-0 w-full">
+            <ConfettiButton onButtonClick={copyEmail}>
+              <span>Get my email</span>
+              {isEmailCopied ? <Check size={16} /> : <CopyIcon size={16} />}
+            </ConfettiButton>
+          </div>
+        </nav>
+      </div>
+    </aside>
+  );
 }
--- src/components/work-card.tsx
+++ src/components/work-card.tsx
@@ -1,77 +1,77 @@
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
-import {Badge} from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 
 interface Props {
+  title: string;
+  description: string;
+  dates: string;
+  position?: string;
+  image?: string;
+  tags?: readonly string[];
+  links?: readonly {
+    icon: React.ReactNode;
     title: string;
-    description: string;
-    dates: string;
-    position: string;
-    image?: string;
-    tags?: readonly string[];
-    links?: readonly {
-        icon: React.ReactNode;
-        title: string;
-        href: string;
-    }[];
+    href: string;
+  }[];
 }
 
 export function WorkCard({
-                             title,
-                             description,
-                             dates,
-                             position,
-                             image,
-                             links,
-                             tags,
-                         }: Props) {
-    return (
-        <div className="relative ml-10 py-4">
-            <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
-                <Avatar className="border size-12 m-auto">
-                    <AvatarImage src={image} alt={title} className="object-contain"/>
-                    <AvatarFallback>{title[0]}</AvatarFallback>
-                </Avatar>
-            </div>
-            <div className="flex flex-1 flex-col justify-start gap-1">
-                {dates && (
-                    <time className="text-xs text-muted-foreground">{dates}</time>
-                )}
-                <h2 className="font-semibold leading-none">{title}</h2>
-                {position && (
-                    <p className="text-sm text-muted-foreground">{position}</p>
-                )}
-                {description && (
-                    <span className="prose dark:prose-invert text-sm text-muted-foreground">
+  title,
+  description,
+  dates,
+  position,
+  image,
+  links,
+  tags,
+}: Props) {
+  return (
+    <div className="relative ml-10 py-4">
+      <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
+        <Avatar className="border size-12 m-auto">
+          <AvatarImage src={image} alt={title} className="object-contain" />
+          <AvatarFallback>{title[0]}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex flex-1 flex-col justify-start gap-1">
+        {dates && (
+          <time className="text-xs text-muted-foreground">{dates}</time>
+        )}
+        <h2 className="font-semibold leading-none">{title}</h2>
+        {position && (
+          <p className="text-sm text-muted-foreground">{position}</p>
+        )}
+        {description && (
+          <span className="prose dark:prose-invert text-sm text-muted-foreground">
             {description}
           </span>
-                )}
-            </div>
-            {tags && tags.length > 0 && (
-                <div className="mt-2 flex flex-wrap gap-1">
-                    {tags?.map((tag) => (
-                        <Badge
-                            className="px-1 py-0 text-[10px]"
-                            variant="secondary"
-                            key={tag}
-                        >
-                            {tag}
-                        </Badge>
-                    ))}
-                </div>
-            )}
-            {links && links.length > 0 && (
-                <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
-                    {links?.map((link, idx) => (
-                        <Link href={link.href} key={idx}>
-                            <Badge key={idx} title={link.title} className="flex gap-2">
-                                {link.icon}
-                                {link.title}
-                            </Badge>
-                        </Link>
-                    ))}
-                </div>
-            )}
+        )}
+      </div>
+      {tags && tags.length > 0 && (
+        <div className="mt-2 flex flex-wrap gap-1">
+          {tags?.map((tag) => (
+            <Badge
+              className="px-1 py-0 text-[10px]"
+              variant="secondary"
+              key={tag}
+            >
+              {tag}
+            </Badge>
+          ))}
         </div>
-    );
+      )}
+      {links && links.length > 0 && (
+        <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
+          {links?.map((link, idx) => (
+            <Link href={link.href} key={idx}>
+              <Badge key={idx} title={link.title} className="flex gap-2">
+                {link.icon}
+                {link.title}
+              </Badge>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
